ba2bbf921b0659069de08f404cdfd3b3dd93a0f8
Munawar <anwarahmad391@gmail.com>
2026-05-04 14:42:09 +0700

    Uji tambah buku dan tampil semua buku di main.ts

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+"use strict";
 // File ini adalah entry point aplikasi
 // Gunakan file ini untuk menguji implementasi yang sudah dibuat
 // Contoh yang bisa dilakukan:
@@ -6,27 +7,22 @@
 //   3. Uji fungsi listBooks untuk melihat semua data
 //   4. Uji fungsi searchBook dengan dan tanpa parameter
 // Silakan bereksplorasi untuk memastikan semua fungsi berjalan dengan baik
-
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Book Management Application - Week 6");
 console.log("=====================================");
-
 // Mulai pengujian di bawah ini
-
-import { addBook, listBooks } from "./functions/bookManager";
-
+const bookManager_1 = require("./functions/bookManager");
 // Tambah data buku
-addBook({
+(0, bookManager_1.addBook)({
     title: "Anne of Green Gables",
     author: "LM Montfomery",
     publicationYear: 1908
 });
-
-addBook({
+(0, bookManager_1.addBook)({
     title: "Heidi",
     author: "Johanna Spyri",
     publicationYear: 1880
 });
-
 // Tampilkan semua buku
 console.log("\n=== LIST SEMUA BUKU ===");
-listBooks();
+(0, bookManager_1.listBooks)();
